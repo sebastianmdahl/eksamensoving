8fd52906d5cd5692c4440d9124b1b0bfc77803a3
sebastianmdahl <sebastian.moen.dahl@hotmail.com>
2025-05-04 20:21:16 +0200

    server

---
+++ server/server.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+const app = new Hono();
+// `serveStatic` makes Hono serve the output from `vite build`
+app.use("*", serveStatic({ root: "../dist" }));
+
+// Heroku provides the port that the server should start on as an environment variable
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+serve({ fetch: app.fetch, port });
